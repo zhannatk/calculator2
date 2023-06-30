1acd5a2dbe2b714cd96cffb32d44e401fbf48aa2
zhannatkachenko <zhanna.kindgirl@gmail.com>
2023-06-30 22:27:05 +0300

    initial commit

---
+++ src/test/java/com/pro/sky/course2/calculator2/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.sky.course2.calculator2.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
